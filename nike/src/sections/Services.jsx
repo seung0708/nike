59bf544325bef56ff6b5b23018ba5bec05c53303
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-06-05 11:53:44 -0700

    started working on the sections/layout

---
+++ nike/src/sections/Services.jsx
@@ -0,0 +1,7 @@
+const Services = () => {
+  return (
+      <div>Test</div>
+     )
+}
+
+export default Services
---
+++ nike/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews'
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffers';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export {
+    Hero, 
+    CustomerReviews,
+    PopularProducts,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
